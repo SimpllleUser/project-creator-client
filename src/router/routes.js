4a27300583e657224b155a76f801221cd5bee25f
SimpllleUser <pecheritsa.vv@gmail.com>
2022-04-19 21:18:55 +0300

    feat(routes): add meta

--- src/router/routes.js
+++ src/router/routes.js
@@ -3,10 +3,16 @@ export const routers = [
         path: '/',
         name: 'Home',
         component: () => import('../views/Home.vue'),
+        meta: {
+          layout: 'default',
+        },
       },
       {
         path: '/about',
         name: 'About',
         component: () => import('../views/About.vue'),
+        meta: {
+          layout: 'empty',
+        },
       }
 ];
