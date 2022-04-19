2b024f7795f53d52d0be2af73ee79fc23ef5d6bb
SimpllleUser <pecheritsa.vv@gmail.com>
2022-04-19 21:17:38 +0300

    feat(LayoutChanger): create

---
+++ src/components/LayoutChanger.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="layout-changer">
+    <component :is="layout">
+      <router-view/>
+    </component>
+  </div>
+</template>
+
+<script>
+import DefaultLayout from '@/layout/DefaultLayout.vue';
+import EmptyLayout from '@/layout/EmptyLayout.vue';
+export default {
+  name: 'LayoutChanger',
+  components: {
+    DefaultLayout,
+    EmptyLayout,
+  },
+  computed: {
+    layout() {
+      return `${this.$route.meta.layout || 'empty'}-layout`;
+    },
+  },
+};
+</script>
---
+++ src/layout/DefaultLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="default-layout">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+};
+</script>
+
+<style scoped>
+</style>
